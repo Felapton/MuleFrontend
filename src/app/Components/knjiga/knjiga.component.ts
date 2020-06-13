d26989d65a7e82f515b888671eb90975ee35d75c
Momcilo <momaface@gmail.com>
2020-06-13 16:35:42 +0100

    CRUD operations defined for Knjiga

--- src/app/Components/knjiga/knjiga.component.ts
+++ src/app/Components/knjiga/knjiga.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { KnjigaService } from '../../Services/knjiga.service';
 import {ActivatedRoute} from '@angular/router';
 import { fromEventPattern } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-knjiga',
@@ -28,6 +29,33 @@ export class KnjigaComponent implements OnInit {
       this.data = this.data.concat(res)
     }, err => {
       console.log(err);
+    });
+  }
+
+  public delete(id, index) {
+    Swal.fire({
+      title: 'Da li ste sigurni?',
+      text: "Kada obrisete necete moci da vratite podatke!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Da, izbrisi!'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Knjiga je uspesno izbrisana!',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.svc.deleteKnjigu(id).subscribe(res => {
+        }, err => {
+          console.log(err);
+        });
+        this.data.splice(index,1);
+      }
     });
   }
 
